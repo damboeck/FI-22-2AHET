a84c2bec36cebdf4e35730e01bb34d3eca793551
Werner <werner.damboeck@letto.at>
2023-05-04 13:32:51 +0200

    AWT Einführung

---
+++ src/at/ac/htlstp/damb/graphisch/Schraffur1.java
@@ -0,0 +1,14 @@
+package at.ac.htlstp.damb.graphisch;
+
+import java.awt.*;
+
+public class Schraffur1 extends Linie1 {
+    public Schraffur1(String name, int breite, int hoehe) {
+        super(name, breite, hoehe);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+    }
+}
