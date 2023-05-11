dd3899967a6d6793babf86d0c678cfa56984c0b3
Werner <werner.damboeck@letto.at>
2023-05-11 11:57:50 +0200

    Muster

---
+++ src/at/ac/htlstp/damb/graphisch/Muster2.java
@@ -0,0 +1,20 @@
+package at.ac.htlstp.damb.graphisch;
+
+import java.awt.*;
+
+public class Muster2 extends FrameCR{
+
+    public static void main(String[] args) {
+        new Muster2();
+    }
+    public Muster2() {
+        super("Muster2");
+        this.setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int d = Integer.min(this.getWidth(),this.getHeight())*8/10;
+        g.drawOval(this.getWidth()/2-d/2, this.getHeight()/2-d/2,d,d);
+    }
+}
--- src/at/ac/htlstp/damb/graphisch/Schraffur1.java
+++ src/at/ac/htlstp/damb/graphisch/Schraffur1.java
@@ -7,8 +7,15 @@ public class Schraffur1 extends Linie1 {
         super(name, breite, hoehe);
     }
 
+    public static void main(String[] args) {
+        new Schraffur1("Schraffur",800,600);
+    }
+
     @Override
     public void paint(Graphics g) {
-
+        int d = Integer.max(this.getWidth(), this.getHeight());
+        // horizontaler bzw. vertikaler Abstand der Linien
+        int a = (this.getWidth()+this.getHeight())/11;
+        for (int x=a-this.getHeight();x<this.getWidth();x+=a) g.drawLine(x,0,x+d,d);
     }
 }
